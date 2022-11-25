988c270a1e41fb01192e5063fda5b0e09489852e
NgyenPHuy <nguyenphuhuy180920@gmail.com>
2022-11-25 17:19:02 +0700

    feat: create banner

--- src/components/Alumni/index.js
+++ src/components/Alumni/index.js
@@ -1,78 +1,51 @@
-import './Alumni.scss'
+import "./Alumni.scss";
+import { useState, useEffect } from "react";
+import { getApiAlumnies } from "@app/config/apiService";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import icon from "@app/assets/images/icons/quote2.webp";
+
 const Alumni = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    arrApiAlumnies();
+  }, []);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+  };
+  const arrApiAlumnies = async () => {
+    let res = await getApiAlumnies();
+    if (res && res.data && res.data.length > 0) {
+      setData(res.data);
+    }
+  };
   return (
     <>
       <div className="alumni container">
         <div className="alumni-title mb-3">
           <h2>What alumni saying</h2>
         </div>
-        <div
-          // className="gallery js-flickity desktop"
-          // data-flickity-options='{ "lazyLoad": 1, "wrapAround": true, "groupCells": true, "autoPlay": true}'
-          // tabIndex="0"
-        >
-          <div className="alumni-saying-detail">
-            <div className="alumni-quote">
-              <img
-                alt="one"
-                className="alumni-quote-icon"
-                src="./images/Alumni/quote2.webp"
-              />
-              <div className="alumni-quote-description">
-                This is an awesome programme which supports me too much in
-                enhancing my skills and knowledge to become a developer. I feel
-                very lucky because of joining Devplus.
-              </div>
-              <div className="alumni-quote-info">
-                <img alt="one" src="./images/Alumni/person1.webp" />
-                <div className="alumni-name">Nghia Le</div>
-                <div className="alumni-position">Member Devplus ++</div>
+        <Slider {...settings}>
+          {data.map((item) => (
+            <div className="alumni-saying-detail" key={item._id}>
+              <div className="alumni-quote">
+                <img alt="one" className="alumni-quote-icon" src={icon} />
+                <div className="alumni-quote-description">{item.saying}</div>
+                <div className="alumni-quote-info">
+                  <img className="image-alumni" alt="one" src={item.image} />
+                  <div className="alumni-name">{item.name}</div>
+                  <div className="alumni-position">{item.position}</div>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="alumni-saying-detail">
-            <div className="alumni-quote">
-              <img
-                alt="one"
-                className="alumni-quote-icon"
-                src="./images/Alumni/quote2.webp"
-              />
-              <div className="alumni-quote-description">
-                I learnt a lot of knowledge from experienced seniors of Dev
-                plus. They help me to understand the procedure. Additionally,
-                taking part in activities such as workshops promote my soft
-                skills.
-              </div>
-              <div className="alumni-quote-info">
-                <img alt="one" src="./images/Alumni/person2.webp" />
-                <div className="alumni-name">Quynh Nga</div>
-                <div className="alumni-position">Member Devplus ++</div>
-              </div>
-            </div>
-          </div>
-          <div className="alumni-saying-detail">
-            <div className="alumni-quote">
-              <img
-                alt="one"
-                className="alumni-quote-icon"
-                src="./images/Alumni/quote2.webp"
-              />
-              <div className="alumni-quote-description">
-                Dev plus help me to re-train about knowledge with technology,
-                experience how to do the real project with senior developers by
-                testing their current project, and share more experience with
-                them. enjoy more events and workshops.
-              </div>
-              <div className="alumni-quote-info">
-                <img alt="one" src="./images/Alumni/person3.webp" />
-                <div className="alumni-name">Thatsadaphone</div>
-                <div className="alumni-position">Member Devplus ++</div>
-              </div>
-            </div>
-          </div>
-        </div>
+          ))}
+        </Slider>
       </div>
-      <div className="frame container px-0"></div>
     </>
   );
 };
